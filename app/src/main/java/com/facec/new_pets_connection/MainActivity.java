62d6652374863b3c028d9c0a191eac5906ea40c7
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-06-14 12:20:54 -0300

    Adicionando slides

--- app/src/main/java/com/facec/new_pets_connection/MainActivity.java
+++ app/src/main/java/com/facec/new_pets_connection/MainActivity.java
@@ -1,16 +1,39 @@
 package com.facec.new_pets_connection;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_3)
+                .canGoForward(false)
+                .build());
+
+//        addSlide(new FragmentSlide.Builder()
+//                .background(R.color.white)
+//                .fragment(R.layout.intro4)
+//                .build());
     }
 }
